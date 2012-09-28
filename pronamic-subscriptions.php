37e81d9292a5db6273f82f642f8b61031278975b
Remco Tolsma <info@remcotolsma.nl>
2012-09-28 09:46:42 +0200

    Made post type hierarchical and support page attributes.

--- pronamic-subscriptions.php
+++ pronamic-subscriptions.php
@@ -44,29 +44,30 @@ class Pronamic_Subscriptions_Plugin {
 		// max. 20 characters, can not contain capital letters or spaces
 		register_post_type( 'pronamic_subs', array(
 			'labels'             => array(
-				'name'               => _x( 'Subscriptions', 'post type general name', 'pronamic_subscriptions' ) , 
-				'singular_name'      => _x( 'Subscription', 'post type singular name', 'pronamic_subscriptions' ) , 
-				'add_new'            => _x( 'Add New', 'pronamic_subscription', 'pronamic_subscriptions' ) , 
-				'add_new_item'       => __( 'Add New Subscription', 'pronamic_subscriptions' ) , 
-				'edit_item'          => __( 'Edit Subscription', 'pronamic_subscriptions' ) , 
-				'new_item'           => __( 'New Subscription', 'pronamic_subscriptions' ) , 
-				'view_item'          => __( 'View Subscription', 'pronamic_subscriptions' ) , 
-				'search_items'       => __( 'Search Subscriptions', 'pronamic_subscriptions' ) , 
-				'not_found'          => __( 'No subscriptions found', 'pronamic_subscriptions' ) , 
-				'not_found_in_trash' => __( 'No subscriptions found in Trash', 'pronamic_subscriptions' ) , 
-				'parent_item_colon'  => __( 'Parent Subscription:', 'pronamic_subscriptions' ) , 
+				'name'               => _x( 'Subscriptions', 'post type general name', 'pronamic_subscriptions' ),
+				'singular_name'      => _x( 'Subscription', 'post type singular name', 'pronamic_subscriptions' ),
+				'add_new'            => _x( 'Add New', 'pronamic_subscription', 'pronamic_subscriptions' ),
+				'add_new_item'       => __( 'Add New Subscription', 'pronamic_subscriptions' ),
+				'edit_item'          => __( 'Edit Subscription', 'pronamic_subscriptions' ),
+				'new_item'           => __( 'New Subscription', 'pronamic_subscriptions' ),
+				'view_item'          => __( 'View Subscription', 'pronamic_subscriptions' ),
+				'search_items'       => __( 'Search Subscriptions', 'pronamic_subscriptions' ),
+				'not_found'          => __( 'No subscriptions found', 'pronamic_subscriptions' ),
+				'not_found_in_trash' => __( 'No subscriptions found in Trash', 'pronamic_subscriptions' ), 
+				'parent_item_colon'  => __( 'Parent Subscription:', 'pronamic_subscriptions' ), 
 				'menu_name'          => _x( 'Subscriptions', 'menu_name', 'pronamic_subscriptions' ) 
 			) , 
-			'public'             => true ,
-			'publicly_queryable' => true ,
-			'show_ui'            => true ,
-			'show_in_menu'       => true ,
-			'query_var'          => true ,
-			'capability_type'    => 'post' ,
-			'has_archive'        => true ,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'capability_type'    => 'post',
+			'has_archive'        => true,
 			'rewrite'            => array( 'slug' => _x( 'subscriptions', 'slug', 'pronamic_subscriptions' ) ) , 
-			'menu_icon'          => plugins_url( 'admin/icons/subscription.png', __FILE__ ) , 
-			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields' ) 
+			'menu_icon'          => plugins_url( 'admin/icons/subscription.png', __FILE__ ) ,
+			'hierarchical'       => true,  
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields', 'page-attributes' ) 
 		));
 	}
 
